d15af05f76a15f8f38a395b0c5e7716b85101224
emrhn-hub <emirhan25.2003@hotmail.com>
2020-04-17 03:12:57 +0300

    first

--- pages/index.js
+++ pages/index.js
@@ -112,7 +112,7 @@ const Home = ({ posts }) => (
 
 Home.getInitialProps = async ({ req }) => {
   // TODO: aşağıdaki satırda bulunan adresi kendi sunucu adresinle değiştirmelisin
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch("https://emrhnakpnr.now.sh/api/posts");
   const json = await res.json();
   return { posts: json.posts };
 };
